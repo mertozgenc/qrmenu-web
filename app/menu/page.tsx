8c657a1189cc45ff72065f9aefd62c8290848bdb
Mert özgenç <mert.ozgencc@gmail.com>
2026-02-06 04:51:29 +0300

    Force dynamic menu page (disable Vercel cache)

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,21 +4,5 @@ export const revalidate = 0;
 import MenuClient from "./MenuClient";
 
 export default function Page() {
-  return (
-    <>
-      <div
-        style={{
-          background: "red",
-          color: "white",
-          padding: "12px",
-          textAlign: "center",
-          fontWeight: "bold",
-        }}
-      >
-        TEST DEPLOY OK 🚀
-      </div>
-
-      <MenuClient />
-    </>
-  );
+  return <MenuClient />;
 }
